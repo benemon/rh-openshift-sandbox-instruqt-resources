2e4b56f7c6ac550c7a5a922320bb6592658c9c76
Ben Holmes <ben.holmes@hashicorp.com>
2024-07-19 14:24:27 +0100

    refactor

--- terraform/aro/main.tf
+++ terraform/aro/main.tf
@@ -2,39 +2,6 @@ resource "azurerm_resource_provider_registration" "this" {
   name = "Microsoft.RedHatOpenShift"
 }
 
-data "azurerm_client_config" "this" {}
-
-data "azuread_client_config" "this" {}
-
-resource "azuread_application" "this" {
-  display_name = "${var.resource_group}-aro"
-}
-
-resource "azuread_service_principal" "this" {
-  client_id = azuread_application.this.client_id
-}
-
-resource "azuread_service_principal_password" "this" {
-  service_principal_id = azuread_service_principal.this.object_id
-}
-
-data "azuread_service_principal" "redhatopenshift" {
-  // This is the Azure Red Hat OpenShift RP service principal id, do NOT delete it
-  client_id = "f1dd0a37-89c6-4e07-bcd1-ffd3d43d8875"
-}
-
-resource "azurerm_role_assignment" "role_network1" {
-  scope                = azurerm_virtual_network.this.id
-  role_definition_name = "Network Contributor"
-  principal_id         = azuread_service_principal.this.object_id
-}
-
-resource "azurerm_role_assignment" "role_network2" {
-  scope                = azurerm_virtual_network.this.id
-  role_definition_name = "Network Contributor"
-  principal_id         = data.azuread_service_principal.redhatopenshift.object_id
-}
-
 resource "azurerm_resource_group" "this" {
   name     = var.resource_group
   location = var.region
