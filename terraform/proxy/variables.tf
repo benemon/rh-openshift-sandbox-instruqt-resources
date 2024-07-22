dff1709a0537b9ec72c4b172287fbffcfa2752ef
Ben Holmes <ben.holmes@hashicorp.com>
2024-07-22 18:24:29 +0100

    added defaults

--- terraform/proxy/variables.tf
+++ terraform/proxy/variables.tf
@@ -6,10 +6,11 @@ variable "instruqt_sandbox_id" {
 variable "public_api_ip" {
   description = "The public IP of the API server"
   type        = string
+  default     = "127.0.0.1"
 }
 
 variable "public_router_ip" {
   description = "The public IP of the Router"
   type        = string
+  default     = "127.0.0.1"
 }
-
